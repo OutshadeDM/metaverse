43899de1af20f02583b2b417eb209d0451df163b
anshrusia200 <anshrusia2002@gmail.com>
2022-07-15 11:25:47 +0530

    all pages made responsive and timer added in marketplace

--- js/Market-place-NFT.js
+++ js/Market-place-NFT.js
@@ -35,3 +35,36 @@ $(document).ready(function () {
     ],
   });
 });
+
+// Set the date we're counting down to
+var countDownDate = new Date().getTime() + 26746000; // millisec value of 7hr 25min 46 sec
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Output the result in an element with id="demo"
+
+  var timers = document.getElementsByClassName("time");
+
+  for (i = 0; i < timers.length; i++) {
+    timers[i].innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
+  }
+
+  // If the count down is over, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    for (i = 0; i < timers.length; i++) {
+      timers[i].innerHTML = "EXPIRED";
+    }
+  }
+}, 1000);
