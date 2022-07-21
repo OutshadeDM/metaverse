38db5a452b38aa8d743fd015f7b6e4c4a44b87b4
anshrusia200 <anshrusia2002@gmail.com>
2022-07-21 15:19:19 +0530

    structure changed

---
+++ js/my-creation.js
@@ -0,0 +1,10 @@
+function more() {
+  document.getElementById("more-btn").style.display = "none";
+  document.getElementById("less-btn").style.display = "block";
+  document.getElementById("cards-2").style.display = "grid";
+}
+function less() {
+  document.getElementById("less-btn").style.display = "none";
+  document.getElementById("more-btn").style.display = "block";
+  document.getElementById("cards-2").style.display = "none";
+}
