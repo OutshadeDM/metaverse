a7bf69ecd9d3ddcb104fb1f13a38c5d721e41d50
anirudh gautam <anirudhgautam987@gmail.com>
2022-08-16 18:22:07 +0530

    animation added to other pages

--- js/blog.js
+++ js/blog.js
@@ -42,3 +42,22 @@ function less() {
   document.getElementById("more-btn").style.display = "block";
   document.getElementById("bottom-2").style.display = "none";
 }
+
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
