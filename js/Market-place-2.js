a92921ca5af0c2f68c438999a369e72ad2d3d1e3
anirudh gautam <anirudhgautam987@gmail.com>
2022-08-18 12:31:43 +0530

    market place changes

--- js/Market-place-2.js
+++ js/Market-place-2.js
@@ -2,7 +2,7 @@ $(".responsive").slick({
   dots: true,
   infinite: false,
   speed: 300,
-  slidesToShow: 5,
+  slidesToShow: 4.5,
   slidesToScroll: 1,
   responsive: [
     {
@@ -86,3 +86,21 @@ window.onclick = function (event) {
     }
   }
 };
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
