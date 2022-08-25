4a6b983f0fca908a6f8436c9bc1c43ef867579a0
anikdev2016 <anikdev2016@gmail.com>
2022-08-25 12:54:40 +0600

    addedd video carousel at home page. not completed but built the structure. and aligned some spacing around the website

--- js/home.js
+++ js/home.js
@@ -16,6 +16,22 @@ function reveal() {
 
 window.addEventListener("scroll", reveal);
 
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
 
 function myFunction() {
   let dropdown = document.getElementById("myDropdown");
